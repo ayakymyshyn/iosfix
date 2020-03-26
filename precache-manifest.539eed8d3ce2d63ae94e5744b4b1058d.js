ec110ff0cb44bc9376e1b3b29b7c91c5e31ea4f5
Andrii Yakymyshyn <andrewyakymyshyn@gmail.com>
2020-03-26 17:07:30 +0300

    Updates

---
+++ precache-manifest.539eed8d3ce2d63ae94e5744b4b1058d.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "305e079c42bc35769ddd556e89022e08",
+    "url": "/iosfix/index.html"
+  },
+  {
+    "revision": "05cc75e9f81670e51246",
+    "url": "/iosfix/static/css/main.7dcf9456.chunk.css"
+  },
+  {
+    "revision": "944df425f10cb2cfe7b7",
+    "url": "/iosfix/static/js/2.43d7e4c5.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/iosfix/static/js/2.43d7e4c5.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "05cc75e9f81670e51246",
+    "url": "/iosfix/static/js/main.e9e7fe4f.chunk.js"
+  },
+  {
+    "revision": "c10766d79a496b226906",
+    "url": "/iosfix/static/js/runtime-main.8c38d4a7.js"
+  }
+]);
